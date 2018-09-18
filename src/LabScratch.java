1538c549831ba729795c6777e0fa6c45298e8791
slim6400 <slim64@illinois.edu>
2018-09-18 16:48:37 -0500

    more work on lab3

--- src/LabScratch.java
+++ src/LabScratch.java
@@ -1,5 +1,27 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+
 public class LabScratch {
     public static void main(String[] unused) {
-        System.out.println("Ran");
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+    }
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
 }
